3f11da388f4801f90353a9dc1b7d6308c71add9b
Juan Carlos Canales <canaleslinaresjuancarlos@gmail.com>
2022-06-10 16:56:03 +0200

    ejercicio 4 SQL

---
+++ ej4.sql
@@ -0,0 +1,32 @@
+INSERT INTO genre VALUES (1, 'Rock');
+INSERT INTO genre VALUES (2, 'Jazz');
+INSERT INTO genre VALUES (3, 'Blues');
+INSERT INTO genre VALUES (4, 'Rap');
+INSERT INTO genre VALUES (5, 'Pop');
+INSERT INTO genre VALUES (6, 'K-Pop');
+INSERT INTO genre VALUES (7, 'HardRock');
+INSERT INTO genre VALUES (8, 'Heavy Metal');
+INSERT INTO genre VALUES (9, 'Reggae');
+INSERT INTO genre VALUES (10, 'Trap');
+
+INSERT INTO Album VALUES (1, 'With the Beatles', '1963-03-22');
+INSERT INTO Album VALUES (2, 'With the Beatles', '1963-03-22');
+INSERT INTO Album VALUES (3, 'Please Please Me', '1963-11-22');
+INSERT INTO Album VALUES (4, 'Meet the Beatles!', '1964-01-20');
+INSERT INTO Album VALUES (5, 'Appetite for Destruction', '1987-07-21');
+INSERT INTO Album VALUES (6, 'Use Your Illusion II', '1993-09-17');
+INSERT INTO Album VALUES (7, 'Chinese Democracy', '2008-11-23');
+INSERT INTO Album VALUES (8, 'Destrangis', '2001-09-24');
+INSERT INTO Album VALUES (9, 'Allenrok', '2008-02-26');
+INSERT INTO Album VALUES (10, 'Rumba a lo desconocido', '2015-10-02');
+
+INSERT INTO  Artist VALUES (1, 'Shakira');
+INSERT INTO  Artist VALUES (2, 'Julio Iglesias');
+INSERT INTO  Artist VALUES (3, 'Macaco');
+INSERT INTO  Artist VALUES (4, 'David Bisbal');
+INSERT INTO  Artist VALUES (5, 'Estopa');
+INSERT INTO  Artist VALUES (6, 'Bad Bunny');
+INSERT INTO  Artist VALUES (7, 'Eladio Carrion');
+INSERT INTO  Artist VALUES (8, 'Kutchi Romero');
+INSERT INTO  Artist VALUES (9, 'Robe Iniesta');
+INSERT INTO  Artist VALUES (10, 'J. R. Kubensis');
