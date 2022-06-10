8a3a5ba72ebb9d0669e631edadd363d0b01d34da
Juan Carlos Canales <canaleslinaresjuancarlos@gmail.com>
2022-06-10 16:21:43 +0200

    ejercicio 3 SQL

---
+++ ej3.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`mitabla` (
+  `idmitabla` INT NOT NULL,
+  `fecha` DATE NULL,
+  `texto` VARCHAR(255) NULL,
+  `entero` INT NULL,
+  `decimal` DECIMAL(2) NULL,
+  PRIMARY KEY (`idmitabla`)
+);
