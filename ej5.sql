b356fb59cf392027efc29a2a5c77c97776a19827
Juan Carlos Canales <canaleslinaresjuancarlos@gmail.com>
2022-06-14 10:00:21 +0200

    subida ejercicios

---
+++ ej5.sql
@@ -0,0 +1,8 @@
+SELECT postalZip as CodigoPostal, region as Provincía, country as País FROM mytable;
+
+SELECT * FROM mytable WHERE phone LIKE '881%';
+
+SELECT * FROM mytable WHERE country IN ('Italy', 'Spain');
+
+SELECT count(*) FROM mytable;
+
